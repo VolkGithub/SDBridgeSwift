7f50854e6201ca7c3e97a1e909a0abfd04fe2eac
housenkui <housenkui@gmail.com>
2022-05-13 20:33:41 +0800

    config Package.swift

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDBridgeSwift",
-            dependencies: [],
-            resources: [.copy("bridge.js"),.copy("hookConsole.js")]),
+            dependencies: []),
         .testTarget(
             name: "SDBridgeSwiftTests",
             dependencies: ["SDBridgeSwift"]),
